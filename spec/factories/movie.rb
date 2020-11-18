e4640b929a366bed362b4a33e94b255e357be25f
Owen Clark <owenthomasclark@me.com>
2020-11-18 14:25:21 +0100

    Add tests for api data and views

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -1,6 +1,8 @@
+MOVIES = ["Kill Bill", "Django", "Kill Bill 2"]
+
 FactoryBot.define do
   factory :movie do
-    title { Faker::Lorem.word }
+    title { MOVIES.sample}
     description { Faker::Lorem.sentence(3, true) }
     released_at { Faker::Date.between(40.years.ago, Time.zone.today) }
     genre
--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -2,9 +2,30 @@ require "rails_helper"
 
 describe "Movies requests", type: :request do
   describe "movies list" do
+    before { create_list(:movie, 5) }
+
     it "displays right title" do
       visit "/movies"
       expect(page).to have_selector("h1", text: "Movies")
+    end
+
+    it "displays the rating for each movie" do
+      visit "/movies"
+      expect(page).to have_selector('.rating', count: 5)
+    end
+  end
+
+  describe "movie show" do
+    let(:movie) { create(:movie) }
+
+    it "displays the movie rating" do
+      visit "/movies/#{movie.id}"
+      expect(page).to have_selector('.rating')
+    end
+
+    it "displays the movie title" do
+      visit "/movies/#{movie.id}"
+      expect(page).to have_content(movie.title)
     end
   end
 end
